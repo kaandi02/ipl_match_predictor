0a38e4d58abf8235308307115b4c293bc126f199
Kaandieepan Sathiyaseelan <52882824+kaandi02@users.noreply.github.com>
2024-04-23 00:02:43 +0530

    Update IPL_Predictor.py

--- IPL_Predictor.py
+++ IPL_Predictor.py
@@ -9,16 +9,89 @@ def get_img_as_base64(file):
     return base64.b64encode(data).decode()
 
 
-img = get_img_as_base64("background.jpeg")
+img = get_img_as_base64("background.jpg")
 # data:image/png;base64,{img}
 page_bg_img = f"""
+
 <style>
+
+[data-baseweb="select"] > div {{
+background-color: white !important;
+color:black !important;
+}}
+
+[data-baseweb="select"] > div > div > div > input{{
+color:black !important;
+}}
+
+[data-baseweb="select"] > div > div > svg{{
+color:black !important;
+}}
+
+[data-testid="stNumberInput"]{{
+outline : none !important;
+}}
+
+[data-testid="stNumberInputContainer"] {{
+height : 50px;
+background-color : white;
+outline : none;
+border: 2px solid black;
+}}
+
+[data-testid="stNumberInputContainer"]:focus-within {{
+border:2px solid blue;
+}}
+
+[data-testid="stNumberInputContainer"] > div > button {{
+outline : none !important;
+color:black;
+background-color:white !important;
+}}
+
+[data-testid="stNumberInputContainer"] > div > button:hover {{
+outline : none !important;
+color:red;
+background-color:green !important;
+}}
+
+[data-testid="stNumberInputContainer"] > div > div{{
+outline : none !important;
+background-color:white;
+}}
+
+[data-testid="stNumberInputContainer"] > div > div > input{{
+font-size:22px;
+color:black;
+}}
+
+[data-testid="stButton"] > button {{
+background-color:transparent !important;
+border: 2px solid white;
+color:white;
+}}
+
+[data-testid="stButton"] > button:hover {{
+background-color:white !important;
+border: none;
+color:black !important;
+border-radius:0px;
+transition : 400ms ease-in-out;
+}}
+
+[data-testid="stButton"] > button:focus {{
+background-color:transparent !important;
+border: 2px solid white !important;
+color:white !important;
+}}
+
 [data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img}");
 width: 100%;
 height:100%
 background-repeat: no-repeat;
 background-attachment: fixed;
-background-size: fit;
+background-size: cover;
 }}
 
 [data-testid="stSidebar"] > div:first-child {{
@@ -37,20 +110,20 @@ right: 2rem;
 
 </style>
 """
-teams =['--- select ---',
-        'Sunrisers Hyderabad',
-        'Mumbai Indians',
-        'Kolkata Knight Riders',
-        'Royal Challengers Bangalore',
-        'Kings XI Punjab',
+teams =['--- Select ---',
         'Chennai Super Kings',
-        'Rajasthan Royals',
         'Delhi Capitals',
+        'Gujarat Titans',
+        'Kolkata Knight Riders',
         'Lucknow Super Giants',
-        'Gujarat Titans']
+        'Mumbai Indians',
+        'Punjab Kings',
+        'Rajasthan Royals',
+        'Royal Challengers Bangalore',
+        'Sunrisers Hyderabad']
 
 
-venues =['--- select ---','Eden Gardens','Wankhede Stadium','M Chinnaswamy Stadium','Feroz Shah Kotla',
+venues =['--- Select ---','Eden Gardens','Wankhede Stadium','M Chinnaswamy Stadium','Feroz Shah Kotla',
 'Dubai International Cricket Stadium','MA Chidambaram Stadium, Chepauk','Sawai Mansingh Stadium',
 'Rajiv Gandhi International Stadium, Uppal','Punjab Cricket Association Stadium, Mohali',
 'Wankhede Stadium, Mumbai','Sheikh Zayed Stadium','Sharjah Cricket Stadium','Dr DY Patil Sports Academy, Mumbai',
@@ -96,9 +169,8 @@ pipe = pickle.load(open('pipe.pkl','rb'))
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown("""
-    # **IPL TEAMS WIN PREDICTION**            
+    # **IPL MATCH PREDICTION**            
 """)
-# st.title("IPL Victory Predictor")
 
 col1, col2 = st.columns(2)
 
@@ -127,22 +199,19 @@ with col3:
     wickets = st.number_input("wicktes down",step=1,min_value=0,max_value=10)
 
 if st.button('Predict Winning Probability'):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    wickets = 10-wickets
-    crr = score/overs
-    rrr = runs_left/(balls_left/6)
-
-    input_data = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],
-                     'venue':[venue],'required_runs':[runs_left],'balls_left':[balls_left],
-                     'wickets_remaining':[wickets],'total_run_x':[target],'crr':[crr],'req_rr':[rrr]})
-   
-    
-    result = pipe.predict_proba(input_data)
-
-    loss = result[0][0]
-    win =  result[0][1]
-    st.header(batting_team + " = "+str(round(win*100)) + "%")
-    st.header(bowling_team + " = "+str(round(loss*100)) + "%")
-    st.header("Current Run Rate = " + str(round(crr)))
-    st.header("Required Run Rate = " + str(round(rrr)))
+    try:
+        runs_left = target - score
+        balls_left = 120 - (overs*6)
+        wickets = 10-wickets
+        crr = score/overs
+        rrr = runs_left/(balls_left/6)
+        input_data = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'venue':[venue],'required_runs':[runs_left],'balls_left':[balls_left],'wickets_remaining':[wickets],'total_run_x':[target],'crr':[crr],'req_rr':[rrr]})
+        result = pipe.predict_proba(input_data)
+        loss = result[0][0]
+        win =  result[0][1]
+        st.header(batting_team + " = "+str(round(win*100)) + "%")
+        st.header(bowling_team + " = "+str(round(loss*100)) + "%")
+        st.header("Current Run Rate = " + str(round(crr)))
+        st.header("Required Run Rate = " + str(round(rrr)))
+    except:
+        st.header("Some error occured. Please check inputs!!")
